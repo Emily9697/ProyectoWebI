b5f0ee11456c06e30b28ecbf92a830f7cf3f80da
Emily9697 <emibricedo@gmail.com>
2021-08-15 21:23:13 -0600

    commit1

---
+++ Casas/Objetos.js
@@ -0,0 +1,14 @@
+const ObCasas = [
+    {
+    Provincia: 'San José',
+    Canton: 'Acosta'
+    },
+    {
+        Provincia: 'San José',
+        Canton: 'Alajuelita'
+    },
+    {
+        Provincia: 'San José',
+        Canton: 'Aserrí'
+    }
+]
